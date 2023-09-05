fbef5d7e8968c58b266273e928d763d444413822
Balal Saleh <balal_saleh@outlook.com>
2023-09-05 21:43:25 +0100

    finishing logic and adding the sound files

---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+let questions = [
+    {
+        "title": "What does HTML stand for?",
+        "choices": ["Hyperlink Text Markup Language", "Hyper Text Markup Language", "Highly Typed Multilayer Language", "Hyper Transfer Markup Language"],
+        "answer": "Hyper Text Markup Language"
+    },
+    {
+        "title": "Which programming language is known for its use in web development and is often used for front-end development?",
+        "choices": ["Java", "Python", "JavaScript", "Ruby"],
+        "answer": "JavaScript"
+    },
+    {
+        "title": "What is the result of 2 + 2 in JavaScript?",
+        "choices": ["3", "4", "5", "6"],
+        "answer": "4"
+    },
+    {
+        "title": "What does CSS stand for?",
+        "choices": ["Cascading Style Sheet", "Computer Style Sheet", "Creative Style Sheet", "Colorful Styling Sheet"],
+        "answer": "Cascading Style Sheet"
+    },
+    {
+        "title": "What is the primary purpose of a 'for' loop in programming?",
+        "choices": ["To declare variables", "To execute a block of code repeatedly", "To define functions", "To create a conditional statement"],
+        "answer": "To execute a block of code repeatedly"
+    },
+    {
+        "title": "Which symbol is used to indicate comments in JavaScript?",
+        "choices": ["//", "/* */", "--", "##"],
+        "answer": "//"
+    },
+    {
+        "title": "What is the result of '5' + 3 in JavaScript?",
+        "choices": ["8", "53", "TypeError", "58"],
+        "answer": "53"
+    },
+    {
+        "title": "What does the 'if' statement do in programming?",
+        "choices": ["It defines a function", "It performs a conditional test", "It creates a loop", "It prints text to the console"],
+        "answer": "It performs a conditional test"
+    },
+    {
+        "title": "Which HTML tag is used to create a hyperlink?",
+        "choices": ["<a>", "<h1>", "<p>", "<link>"],
+        "answer": "<a>"
+    },
+    {
+        "title": "What does the acronym 'SQL' stand for in the context of databases?",
+        "choices": ["Structured Query Language", "Simple Query Logic", "System Question Language", "Structured Query Logic"],
+        "answer": "Structured Query Language"
+    }
+];
